f44017c80661a917e8d825e98378570a7625d6c4
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-07-19 01:51:22 +0300

    checkers field with items

--- src/components/Checkers/styled.ts
+++ src/components/Checkers/styled.ts
@@ -3,36 +3,100 @@ import { IStyled } from 'common/types/styledTypes';
 
 export const StContainer = styled.div`
   width: 100vw;
-  height: calc(100vh - 90px);
 `;
 
-export const StTable = styled.table < IStyled > `
-  display: table;
-  border-collapse: separate;
-  text-indent: initial;
-  border-spacing: 2px;
-
-  width: 444px;
-  height: 444px;
-  border: 2px solid red;
-  margin: 140px auto;
+export const StTable = styled.div < IStyled > `
+  display: flex;
+  flex-direction: row-reverse;
+  flex-wrap: wrap;
+  overflow: hidden;
+  width: 660px;
+  height: 660px;
+  border: 2px solid ${({ colors, theme }) => colors[theme].textColor};
+  margin: 45px auto;
   background: ${({ colors, theme }) => colors[theme].backgroundSecondary};
+  transform: rotate(180deg);
+  
 `;
-export const StTbody = styled.table`
-  display: table-row-group;
-  vertical-align: middle;
-  /* border-color: inherit; */
-  width: 400px;
-  height: 400px;
-  border: 1px solid #fff;
-`;
-export const StTd = styled.td`
-  text-align: center;
-  vertical-align: middle;
-  width: 50px;
-  border: 1px solid #fff;
-  color: #fff;
-  &::first-child{
+export const StCell = styled.div < IStyled > `
+  width: 80px;
+  height: 80px;
+  border: 1px solid ${({ colors, theme }) => colors[theme].textColor};
+  margin: 1px;
+  color: ${({ colors, theme }) => colors[theme].textColor};
+  background: #ccc;
+  transform: rotate(180deg);
+  
+  &:nth-child(2),
+  &:nth-child(4),
+  &:nth-child(6),
+  &:nth-child(8),
+  &:nth-child(9),
+  &:nth-child(11),
+  &:nth-child(13),
+  &:nth-child(15),
+  &:nth-child(18),
+  &:nth-child(20),
+  &:nth-child(22),
+  &:nth-child(24),
+  &:nth-child(25),
+  &:nth-child(27),
+  &:nth-child(29),
+  &:nth-child(31),
+  &:nth-child(34),
+  &:nth-child(36),
+  &:nth-child(38),
+  &:nth-child(40),
+  &:nth-child(41),
+  &:nth-child(43),
+  &:nth-child(45),
+  &:nth-child(47),
+  &:nth-child(50),
+  &:nth-child(52),
+  &:nth-child(54),
+  &:nth-child(56),
+  &:nth-child(57),
+  &:nth-child(59),
+  &:nth-child(61),
+  &:nth-child(63)
+  {
     background: #fff;
+    & div {
+      content: '';
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 40px;
+      height: 40px;
+      border: 2px solid #000;
+      box-shadow: 0 0 2px 2px #000;
+      border-radius: 50%;
+      background: #fff;
+      cursor: grab;
+      &::after {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 25px;
+        height: 25px;
+        border: 2px solid #000;
+        border-radius: 50%;
+        background: #fff;
+      }
+    }
   }
+`;
+export const StChecker = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50px;
+  height: 50px;
+  border: 2px solid darkred;
+  border-radius: 50%;
+  background: red;
+  cursor: grab;
 `;
---
+++ src/constants/component.ts
@@ -0,0 +1,258 @@
+export const CELL = [
+  {
+    id: 1,
+    color: 'black',
+  },
+  {
+    id: 2,
+    color: 'white',
+  },
+  {
+    id: 3,
+    color: 'black',
+  },
+  {
+    id: 4,
+    color: 'white',
+  },
+  {
+    id: 5,
+    color: 'black',
+  },
+  {
+    id: 6,
+    color: 'white',
+  },
+  {
+    id: 7,
+    color: 'black',
+  },
+  {
+    id: 8,
+    color: 'white',
+  },
+  {
+    id: 9,
+    color: 'black',
+  },
+  {
+    id: 10,
+    color: 'white',
+  },
+  {
+    id: 11,
+    color: 'black',
+  },
+  {
+    id: 12,
+    color: 'white',
+  },
+  {
+    id: 13,
+    color: 'black',
+  },
+  {
+    id: 14,
+    color: 'white',
+  },
+  {
+    id: 15,
+    color: 'black',
+  },
+  {
+    id: 16,
+    color: 'white',
+  },
+  {
+    id: 17,
+    color: 'black',
+  },
+  {
+    id: 18,
+    color: 'white',
+  },
+  {
+    id: 19,
+    color: 'black',
+  },
+  {
+    id: 20,
+    color: 'white',
+  },
+  {
+    id: 21,
+    color: 'black',
+  },
+  {
+    id: 22,
+    color: 'white',
+  },
+  {
+    id: 23,
+    color: 'black',
+  },
+  {
+    id: 24,
+    color: 'white',
+  },
+  {
+    id: 25,
+    color: 'black',
+  },
+  {
+    id: 26,
+    color: 'white',
+  },
+  {
+    id: 27,
+    color: 'black',
+  },
+  {
+    id: 28,
+    color: 'white',
+  },
+  {
+    id: 29,
+    color: 'black',
+  },
+  {
+    id: 30,
+    color: 'white',
+  },
+  {
+    id: 31,
+    color: 'black',
+  },
+  {
+    id: 32,
+    color: 'white',
+  },
+  {
+    id: 33,
+    color: 'black',
+  },
+  {
+    id: 34,
+    color: 'white',
+  },
+  {
+    id: 35,
+    color: 'black',
+  },
+  {
+    id: 36,
+    color: 'white',
+  },
+  {
+    id: 37,
+    color: 'black',
+  },
+  {
+    id: 38,
+    color: 'white',
+  },
+  {
+    id: 39,
+    color: 'black',
+  },
+  {
+    id: 40,
+    color: 'white',
+  },
+  {
+    id: 41,
+    color: 'black',
+  },
+  {
+    id: 42,
+    color: 'white',
+  },
+  {
+    id: 43,
+    color: 'black',
+  },
+  {
+    id: 44,
+    color: 'white',
+  },
+  {
+    id: 45,
+    color: 'black',
+  },
+  {
+    id: 46,
+    color: 'white',
+  },
+  {
+    id: 47,
+    color: 'black',
+  },
+  {
+    id: 48,
+    color: 'white',
+  },
+  {
+    id: 49,
+    color: 'black',
+  },
+  {
+    id: 50,
+    color: 'white',
+  },
+  {
+    id: 51,
+    color: 'black',
+  },
+  {
+    id: 52,
+    color: 'white',
+  },
+  {
+    id: 53,
+    color: 'black',
+  },
+  {
+    id: 54,
+    color: 'white',
+  },
+  {
+    id: 55,
+    color: 'black',
+  },
+  {
+    id: 56,
+    color: 'white',
+  },
+  {
+    id: 57,
+    color: 'black',
+  },
+  {
+    id: 58,
+    color: 'white',
+  },
+  {
+    id: 59,
+    color: 'black',
+  },
+  {
+    id: 60,
+    color: 'white',
+  },
+  {
+    id: 61,
+    color: 'black',
+  },
+  {
+    id: 62,
+    color: 'white',
+  },
+  {
+    id: 63,
+    color: 'black',
+  },
+  {
+    id: 64,
+    color: 'white',
+  },
+];
