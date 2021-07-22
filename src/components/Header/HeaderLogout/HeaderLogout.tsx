b8422c3f5abb38bbccdb2cca19c1c29193ec3aeb
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-07-22 15:10:16 +0300

    exit logic

--- src/components/Header/HeaderLogout/HeaderLogout.tsx
+++ src/components/Header/HeaderLogout/HeaderLogout.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { client } from 'constants/urls';
 import { useTheme } from 'src/components/hooks/useTheme';
+import { cookieMaster } from 'src/helpers/cookieMaster';
 import { StDoor } from './styled';
 
 const HeaderLogout = () => {
     const { colors, theme } = useTheme();
+    const handleExit = () => {
+        cookieMaster.deleteCookie('token');
+    };
     return (
-        <NavLink to={client.authClient}>
+        <NavLink onClick={handleExit} to={client.authClient}>
             <StDoor
                 src={client.logo}
                 theme={theme}
