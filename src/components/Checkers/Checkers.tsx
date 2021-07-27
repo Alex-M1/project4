868965e48622e1b4bc170ab8c1b01e7f8562e923
Алексей Малик <alex94.malik@gmail.com>
2021-07-27 20:49:30 +0300

    add checker test

--- src/components/Checkers/Checkers.tsx
+++ src/components/Checkers/Checkers.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 import { CHECKER_FIELD_INIT, CHESS_DESK } from 'constants/constants';
 import { ICheckerModel } from 'store/checkers/types';
 import { useTheme } from '../hooks/useTheme';
@@ -22,28 +20,27 @@ const Checkers: React.FC<IProps> = ({ refreshField, connectCheckersChannel }) =>
   const cols = CHESS_DESK.COLS;
   let cellNumber = CHESS_DESK.CELLS_NUM_INIT;
   return (
-    <DndProvider backend={HTML5Backend}>
-      <StContainer>
-        <StTable
-          theme={theme}
-          colors={colors}
-        >
-          {
-            rows.map((row) => {
-              return cols.map((col) => {
-                cellNumber++;
-                return (
-                  <Cell
-                    key={`row_${row}_col_${col}`}
-                    cellNumber={cellNumber}
-                  />
-                );
-              });
-            })
-          }
-        </StTable>
-      </StContainer>
-    </DndProvider>
+    <StContainer>
+      <StTable
+        theme={theme}
+        colors={colors}
+      >
+        {
+          
+          rows.map((row) => {
+            return cols.map((col) => {
+              cellNumber++;
+              return (
+                <Cell
+                  key={`row_${row}_col_${col}`}
+                  cellNumber={cellNumber}
+                />
+              );
+            });
+          })
+        }
+      </StTable>
+    </StContainer>
   );
 };
 export default Checkers;
